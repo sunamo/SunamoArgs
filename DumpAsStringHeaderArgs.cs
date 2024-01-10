b927e38d536ea6beb235613e62a5fe423ea8ea33
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:46:26 +0100

    .

---
+++ DumpAsStringHeaderArgs.cs
@@ -0,0 +1,15 @@
+namespace SunamoArgs;
+
+/// <summary>
+///     Mus� b�t v SunamoArgs proto�e je sd�lena ve SunamoReflection i SunamoCollectionWithoutDuplicates
+/// </summary>
+public class DumpAsStringHeaderArgs
+{
+    public static DumpAsStringHeaderArgs Default = new();
+
+    /// <summary>
+    ///     Only names of properties to get
+    ///     If starting with ! => surely delete
+    /// </summary>
+    public List<string> onlyNames = new();
+}
