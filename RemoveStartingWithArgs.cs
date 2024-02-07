7c18ab0279a05eb49af237f1bbf2287351e75496
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-02-07 05:28:08 +0100

    .

---
+++ RemoveStartingWithArgs.cs
@@ -0,0 +1,10 @@
+namespace SunamoCollectionsShared.Args;
+
+public class RemoveStartingWithArgs
+{
+    /// <summary>
+    /// Original
+    /// </summary>
+    public bool _trimBeforeFinding = false;
+    public bool caseSensitive = true;
+}
