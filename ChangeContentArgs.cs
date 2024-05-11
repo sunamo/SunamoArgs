b6b436dcf6437fe449352c5e4860cf2906455960
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:17 +0200

    Removed usings outside of projects

--- ChangeContentArgs.cs
+++ ChangeContentArgs.cs
@@ -1,3 +1,5 @@
+
+
 namespace
 #if SunamoDevCode
 SunamoDevCode
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,7 +2,8 @@
 global using System.Collections.Generic;
 global using System;
 global using System.Text;
-global using System.Collections.Generic;
 global using System.Collections;
 global using System.IO;
 global using System.Linq;
+global using SunamoArgs;
+
