da60f8263c351914335f04c9ed06965799e33b38
sunamo.cz <radek.jancik@sunamo.cz>
2024-08-03 08:11:45 +0200

    Renamed properties

--- RemoveStartingWithArgs.cs
+++ RemoveStartingWithArgs.cs
@@ -4,6 +4,7 @@ public class RemoveStartingWithArgs
     /// <summary>
     /// Original
     /// </summary>
-    public bool _trimBeforeFinding = false;
-    public bool caseSensitive = true;
+    public bool TrimBeforeFinding { get; } = false;
+
+    public bool CaseSensitive { get; } = true;
 }
