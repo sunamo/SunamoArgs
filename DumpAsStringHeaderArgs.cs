fd967e2c11ffa6cb606abac7bd0b17ff5b2ba4a0
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-24 07:27:34 +0200

    25.7.24.2 Added test code

--- DumpAsStringHeaderArgs.cs
+++ DumpAsStringHeaderArgs.cs
@@ -7,6 +7,17 @@ public class DumpAsStringHeaderArgs
 {
     public static DumpAsStringHeaderArgs Default { get; set; } = new();
 
+    public DateTime Test_ToDelete()
+    {
+        return Test_ToDelete_Inner();
+    }
+
+    private DateTime Test_ToDelete_Inner()
+    {
+
+        return DateTime.Today;
+    }
+
     /// <summary>
     ///     Only names of properties to get
     ///     If starting with ! => surely delete
