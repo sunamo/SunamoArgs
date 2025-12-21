008a1153d3f5d35c515702b9a2e9b60fb56a40f0
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 18:31:15 +0100

    25.12.21.1 feat: Upgrade to net10.0

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 
-namespace SunamoRegex.Tests;
+namespace SunamoArgs;
 
 internal class Program
 {
