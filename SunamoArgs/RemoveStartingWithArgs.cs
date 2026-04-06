71a3f46defb951f460d472af9ae68a7da332e88f
Radek Jancik <radek.jancik@usys.cz>
2026-04-06 14:53:42 +0200

    26.4.6.5 feat: Improved code quality

--- SunamoArgs/RemoveStartingWithArgs.cs
+++ SunamoArgs/RemoveStartingWithArgs.cs
@@ -1,17 +1,17 @@
 namespace SunamoArgs;
 
 /// <summary>
-/// Arguments for remove starting with operations.
+/// Arguments for remove-starting-with operations.
 /// </summary>
 public class RemoveStartingWithArgs
 {
     /// <summary>
     /// Gets or sets a value indicating whether to trim whitespace before finding the starting pattern.
     /// </summary>
-    public bool TrimBeforeFinding { get; set; } = false;
+    public bool IsTrimBeforeFinding { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether the search is case sensitive.
     /// </summary>
-    public bool CaseSensitive { get; set; } = true;
+    public bool IsCaseSensitive { get; set; } = true;
 }
